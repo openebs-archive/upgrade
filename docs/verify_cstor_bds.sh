7980b8820a6b55a5f1d7d85b1534f0cb2b1f07ee
shubham <shubham.bajpai@mayadata.io>
2020-08-10 22:23:32 +0530

    update script with latest approach  - the script iterates over the bd in csp and    if bd is inactive then look for its corresponding    index devlink and find the new bd
    Signed-off-by: shubham <shubham.bajpai@mayadata.io>

--- docs/verify_cstor_bds.sh
+++ docs/verify_cstor_bds.sh
@@ -3,41 +3,72 @@ ns=$1
 if [[ $ns == "" ]]; then
     ns="openebs"
 fi 
-cspList=$(kubectl get csp -o jsonpath='{.items[*].metadata.name}')
-bdList=$(kubectl -n $ns get blockdevices -o jsonpath='{.items[*].metadata.name}')
 
+find_bd_for_devlink() {
+        devlink=$1
+        bdList=$(kubectl -n $ns get blockdevices -o jsonpath='{.items[*].metadata.name}')
+        for bd in $bdList
+        do
+                links=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.spec.devlinks[*].links}")
+                # remove the list [] from by-id/by-path links output
+                links=$(echo $links | tr -d [ | tr -d ])
+                if [[ $links == "" ]]; then
+                        links=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.spec.path}")
+                fi
+                for link in $links
+                do
+                        if [[ "$devlink" == *"$link"* ]]; then
+                                state=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.status.state}")
+                                claimState=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.status.claimState}")
+                                if [[ $state == "Active" && $claimState == "Unclaimed" ]]; then
+                                        echo "$bd"
+                                        break
+                                fi
+                        fi
+                done
+        done
+}
+
+cspList=$(kubectl get csp -o jsonpath='{.items[*].metadata.name}')
 for csp in $cspList
 do
         echo "Verifying blockdevices on $csp"
         pod=$(kubectl -n $ns get pods -l openebs.io/cstor-pool=$csp -o jsonpath="{.items[?(@.status.phase=='Running')].metadata.name}")
-        devlinks=$(kubectl -n $ns exec -it $pod -c cstor-pool -- zpool status -P | grep dev | awk '{print $1}')
-        for devlink in $devlinks
+        devlinks=$(kubectl -n $ns exec -it $pod -c cstor-pool -- zpool status -P | grep \/dev | awk '{print $1}')
+        cspBDs=$(kubectl get csp $csp -o jsonpath="{.spec.group[*].blockDevice[*].name}")
+        bdIndex="0"
+        for bd in $cspBDs
         do
-                oldbd=""
+                bdIndex=$(($bdIndex+1))
+                oldbd=$bd
                 newbd=""
-                for bd in $bdList
+                state=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.status.state}")
+                claimState=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.status.claimState}")
+                if [[ $state == "Active" && $claimState == "Claimed" ]]; then
+                        nodeName=$(kubectl -n $ns get bd $bd -o jsonpath="{.spec.nodeAttributes.nodeName}")
+                        node=$(kubectl get node $nodeName)
+                        if [ $? -eq 0 ]; then
+                                continue
+                        fi
+                fi
+
+                devIndex="0"
+                for devlink in $devlinks
                 do
-                        links=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.spec.devlinks[?(@.kind=='by-id')].links}")
-                        # remove the list [] from by-id/by-path links output
-                        links=$(echo $links | tr -d [ | tr -d ])
-                        for link in $links
-                        do
-                                if [[ "$devlink" == *"$link"* ]]; then
-                                        state=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.status.state}")
-                                        claimState=$(kubectl -n $ns get blockdevices $bd -o jsonpath="{.status.claimState}")
-                                        if [[ $claimState == "Claimed" ]]; then
-                                                oldbd=$bd
-                                        elif [[ $state == "Active" && $claimState == "Unclaimed" ]]; then
-                                                newbd=$bd
-                                        fi
+                        devIndex=$(($devIndex+1))
+                        if [ $bdIndex == $devIndex ]; then
+                                newbd=$(find_bd_for_devlink "$devlink")
+                                if [[ $newbd != "" ]]; then
+                                        # if new blockdevice found after reattach deplay the old and new name
+                                        echo "Please update $csp blockdevice from $oldbd --> $newbd"
+                                else
+                                        # if no new blockdevice found for old, put a warning in red
+                                        echo "$(tput setaf 1)For $csp inactive blockdevice $oldbd does not have an active blockdevice$(tput sgr0)"
                                 fi
-                        done
-                        if [[ $oldbd != "" && $newbd != "" ]]; then
                                 break
                         fi
                 done
-                if [[ $oldbd != "" && $newbd != "" ]]; then
-                        echo "Please update $csp blockdevice from $oldbd --> $newbd"
-                fi
         done
 done
+
+
